cd32cd9014c6638e66909025ffb7022b06eb7184
Василий Борцов <s.t.a.l.k.e.r.bva@gmail.com>
2022-10-08 14:30:10 +0300

    Seminar_004

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -69,3 +69,135 @@ for (int i = 1; i <= N; i++)
     Console.WriteLine("{0}^3 = {1}", i, i*i*i);
 }
 */
+/*  Напишите программу, которая принимает на вход число (А) 
+и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36  
+
+int a = 0;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+Console.Write($"Сумма чисел от 1 до {a} = {Sum(a)}");
+
+int Sum(int _a)
+{
+    int sum = 0;
+    for (int i = 1; i <= _a; i++)
+        sum += i;
+    return sum;
+}
+
+int Max(int a, int b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+
+double Div(int a, int b)
+{
+    return (double)a / (double)b;
+}
+
+string Output(string text)
+{
+    return $"+{text}+";
+}
+
+bool IsEven(int a)
+{
+    if (a % 2 == 0)
+        return true;
+    return false;
+}
+
+int[] CreateArray(int size)
+{
+    int[] _array = new int[size];
+    return _array;
+}
+
+void print(string _text)
+{
+    Console.Write(_text);
+    return;
+}
+
+print($"Это использование функции void");
+
+int G_i = 0;
+string G_str = "";
+
+Max(5);
+
+Console.Write($"{G_i} {G_str}");
+
+void Max(int a)
+{
+    G_i = a;
+    if (a > 0)
+        G_str = "переменная положительна";
+    else if (a < 0)
+        G_str = "переменная отрицательна";
+    else
+        G_str = "переменная равна 0";
+    Min(a);
+}
+
+void Min(int a)
+{
+    G_i *= -1;
+}
+*/
+/*     Напишите программу, которая принимает на вход число N 
+и выдаёт произведение чисел от 1 до N.
+4  -> 24
+5  -> 120  
+
+int N = 0;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+Console.Write($"Произведение чисел от 1 до {N} = {Factorial(N)}");
+
+int Factorial(int _N)
+{
+    int factorial = 1;
+    for (int i = 1; i <= _N; i++)
+        factorial *= i;
+    return factorial;
+}
+ */
+/*     Напишите функцию, которая выводит массив из 8 элементов,
+заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]   
+
+int[] array;
+
+array = CreateArray();
+for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+
+int[] CreateArray()
+{
+    int[] _array = new int[8];
+    for (int i = 0; i < 8; i++)
+        _array[i] = new Random().Next(0, 2);
+    return _array;
+}
+
+int[] array = new int[20];
+
+foreach (var item in CreateArray(20))
+    Console.Write($"{item} ");
+
+int[] CreateArray(int _size)
+{
+    int[] _array = new int[_size];
+    for (int i = 0; i < _size; i++)
+        _array[i] = new Random().Next(10);
+    return _array;
+}
+*/
---
+++ Seminar4/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+
+int a = 0;
+int b = 0;
+int pow = 1;
+Console.Write($"Введите число A: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Введите число B: ");
+int.TryParse(Console.ReadLine()!, out b);
+for (int i = 0; i < b; i++) {
+        pow *= a;
+    }
+Console.Write($"Возведение число A в натуральную степень B = {pow}");       
+return 0;
+
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+
+int a = 0;
+Console.Write($"Введите число A: ");
+int.TryParse(Console.ReadLine()!, out a);
+int s = 0;
+    while (a > 0)
+        {
+            s = s + a % 10;
+            a = a /10 ;
+ 
+        }
+ Console.Write($"Сумма цифр в числе = {s}"); 
+
+Задача 29: Напишите функцию, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+
+int i;
+int[] a = new int[10];
+Console.WriteLine("Введите массив чисел: ");
+for (i = 0; i < 10; i++)
+
+a[i] = int.Parse(Console.ReadLine());
+
+ 
+for (i = 0; i < 10; i++)
+
+Console.WriteLine(a[i]);
+*/
