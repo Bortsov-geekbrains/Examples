e434f51af55f0e91a404d5c2e2612b35dc8f7dfe
Василий Борцов <s.t.a.l.k.e.r.bva@gmail.com>
2022-10-18 11:29:00 +0300

    Seminar_009

--- Seminar8/Program.cs
+++ Seminar8/Program.cs
@@ -311,3 +311,101 @@ void PrintArray(int[,] array)
     }
 }
 */
+/*          **Задача 63:** Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"           
+
+int a;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+PrintNumbers(1, a);
+
+void PrintNumbers(int _start, int _end)
+{
+    if (_start == _end)
+    {
+        Console.Write($"{_start}");
+        PrintNumbers(_start + 1, _end);
+    }
+}
+*/
+/*          **Задача 65:** Задайте значения M и N. Напишите программу, которая 
+выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> "1, 2, 3, 4, 5"
+M = 4; N = 8. -> "4, 5, 6, 7, 8"           
+
+int a;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+int b;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+Console.Write(PrintNumbers(a, b));
+
+string PrintNumbers(int _start, int _end)
+{
+    if (_start == _end)
+        return _start.ToString();
+    return $"{_start} {PrintNumbers(_start + 1, _end)}";
+}
+*/
+/*          **Задача 63:** Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"           
+
+int a;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+PrintNumbers(1, a);
+
+void PrintNumbers(int _start, int _end)
+{
+    if (_start != _end)
+    {
+        Console.Write($"{_start}");
+        PrintNumbers(_start + 1, _end);
+    }
+    else
+        Console.Write($"{_end}");
+}
+*/
+/*          **Задача 67:** Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+            453 -> 12
+            45 -> 9           
+
+int a;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+Console.Write($"{DigitsSum(a)}");
+
+int DigitsSum(int a)
+{
+    if (a == 0) return 0;
+    return a % 10 + DigitsSum(a / 10);
+}
+*/
+/*          **Задача 69:** Напишите программу, которая на вход принимает два числа A и B, 
+            и возводит число А в целую степень B с помощью рекурсии.
+            A = 3; B = 5 -> 243 (3⁵)
+            A = 2; B = 3 -> 8           
+
+int a, b;
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+Console.Write($"{Pow(a, b)}");
+
+int Pow (int _a, int _b)
+{
+    if (_b < 1)
+        return 1;
+    return _a * Pow(_a, _b - 1);
+}
+*/
---
+++ Seminar9/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int number;
+Console.WriteLine("Введите натуральное число больше 1:");
+int.TryParse(Console.ReadLine()!, out number);
+
+///Метод вывода натуральных чисел от N до 1:
+void NumberCounter (int _number)
+{
+    if (_number < 0) Console.Write($"{_number} не натуральное число");
+    if (_number == 0) return;
+    Console.Write("{0,4}", _number);
+    NumberCounter (_number - 1);
+}
+
+NumberCounter(number);
+
+
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+
+int M;
+int N;
+Console.Write("M = ");
+int.TryParse(Console.ReadLine()!, out M);
+Console.Write("N = ");
+int.TryParse(Console.ReadLine()!, out N);
+Console.WriteLine($"Result, S = {SumNumbers(M, N)}");
+Console.ReadLine();
+ 
+static int SumNumbers(int M, int N)
+{
+    if (M == 0) return (N * (N + 1)) / 2;            // Если M равно нулю
+    else if (N == 0) return (M * (M + 1)) / 2;       // Если N равно нулю
+    else if (M == N) return M;                       // Если M=N
+    else if (M < N) return N + SumNumbers(M, N - 1); // Если M<N
+    else return N + SumNumbers(M, N + 1);            // Если M>N
+}
+
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+
+int n;
+int m;
+int.TryParse(Console.ReadLine()!, out m);
+int.TryParse(Console.ReadLine()!, out n);
+
+Console.Write(Ack(m, n)); // вызов рекурсивной функции    
+
+static int Ack(int _m, int _n)
+{
+    if (_m == 0) return _n + 1;
+
+    else if (_n == 0) return Ack(_m - 1, 1);
+
+    else return Ack(_m - 1, Ack(_m, _n - 1));
+
+}
+*/
+
+
+
+
+
+
